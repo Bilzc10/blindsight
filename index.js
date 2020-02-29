9a470185fba0bca374df0a0d93a8578a7baef0c9
PaulieB <36047697+Paulieb821@users.noreply.github.com>
2020-02-29 13:57:04 -0500

    69

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = require('http').Server(app);
 const fs = require('fs');
 const path = require('path');
 
-server.listen(80);
+server.listen(69);
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
